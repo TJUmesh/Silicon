4d0698859bd3c5d5c4c883cd46476c086e455e62
Umesh Kumarclear <umesh@technojade.com>
2024-06-19 13:09:42 +0530

    This is new Feature

--- src/main/java/com/demo2/Controller.java
+++ src/main/java/com/demo2/Controller.java
@@ -1,10 +1,18 @@
 package com.demo2;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/demo")
 public class Controller {
 
 	//this is Controller class
+	@GetMapping
+	public String demo()
+	{
+		return "This is Spring Boot Code";
+	}
 	
 }
